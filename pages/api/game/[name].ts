b9717c55564ddbb5348141f2c982319a42e400c0
Baruch Velez <baruchvelez@gmail.com>
2021-03-12 14:48:10 +0100

    chore: add husky for precommit linting

--- pages/api/game/[name].ts
+++ pages/api/game/[name].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 type GameNameRequest = NextApiRequest & { query: { name: string } };
 
-export default function gameNameHanler({ query: { name } }: GameNameRequest, res: NextApiResponse) {
+export default function gameNameHandler({ query: { name } }: GameNameRequest, res: NextApiResponse) {
   if (!name) {
     res.status(404).json({ message: "No game found." });
   } else {
